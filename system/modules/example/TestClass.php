504d731a3f727241e9423853439510e5d79d4ea9
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2013-10-16 13:33:04 +0200

    Add example class

---
+++ system/modules/example/TestClass.php
@@ -0,0 +1,33 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * My own project
+ *
+ * PHP version 5
+ * @copyright  John Doe 2013
+ * @author     John Doe
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * Class TestClass
+ *
+ * Clarify the system.
+ * @copyright  John Doe 2013
+ * @author     John Doe
+ * @package    Example
+ */
+class TestClass extends Controller
+{
+
+	/**
+	 * Example function
+	 * @return string
+	 */
+	protected function example()
+	{
+		return 'Hello World!';
+	}
+}
